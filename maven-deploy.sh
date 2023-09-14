4da371493329e25eeba0f8fcebbd04437f931d26
shubham-tewari <shubham.tewari@khoros.com>
2023-09-14 15:53:21 +0530

    MOBL-8557: changes for maven central

---
+++ maven-deploy.sh
@@ -0,0 +1,11 @@
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dversion=1.14.3 -Dpackaging=aar -DpomFile=brandmessengercommons-1.14.3.pom -Dfile=brandmessengercommons-1.14.3.aar
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengercommons-1.14.3.pom -Dfile=brandmessengercommons-1.14.3-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengercommons-1.14.3.pom -Dfile=brandmessengercommons-1.14.3-javadoc.jar -Dclassifier=javadoc
+
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dversion=1.14.3 -Dpackaging=aar -DpomFile=brandmessengercore-1.14.3.pom -Dfile=brandmessengercore-1.14.3.aar
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengercore-1.14.3.pom -Dfile=brandmessengercore-1.14.3-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengercore-1.14.3.pom -Dfile=brandmessengercore-1.14.3-javadoc.jar -Dclassifier=javadoc
+
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dversion=1.14.3 -Dpackaging=aar -DpomFile=brandmessengerui-1.14.3.pom -Dfile=brandmessengerui-1.14.3.aar
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengerui-1.14.3.pom -Dfile=brandmessengerui-1.14.3-sources.jar -Dclassifier=sources
+mvn gpg:sign-and-deploy-file -Durl=https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/ -DrepositoryId=ossrh -Dpackaging=jar -DpomFile=brandmessengerui-1.14.3.pom -Dfile=brandmessengerui-1.14.3-javadoc.jar -Dclassifier=javadoc
